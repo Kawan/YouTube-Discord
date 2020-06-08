faa2b444292948d0aab18f186febd5aaadac119f
Kawan <43068452+Kawan@users.noreply.github.com>
2020-06-09 01:17:42 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -3,28 +3,6 @@ import json
 import sys
 from os import system
 
-system("title "+'Discord Webhook Advanced by Kawan')
-
-file = open('passwords.txt', 'r')
-CodeBreacher = file.readlines()
-stripped_codes = [s.rstrip() for s in CodeBreacher]
-
-x = True
-
-while x == True:
-    passcode = input("Enter your desgire pin to continue: ")
-    if passcode in stripped_codes:
-        print("You accessed the database!")
-        x == False
-        break
-    else:
-        print("Wrong password!")
-        answer = input('Do you want to try again? (y/n)')
-        if answer.lower() == 'y':
-            x == True
-        else:
-            x == False
-            sys.exit("Database Closed!")
 name = input('Enter name: ')
 choice = input("What you want to post? (video/comment/livestream)")
 
